76f8c70524fb10fef40744b3b28d83d1581335f2
Ynnk <y.houssin@live.fr>
2018-12-06 12:08:42 +0100

    adding stuffs and removing stuffs

--- lib/main.dart
+++ lib/main.dart
@@ -14,23 +14,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+  MyHomePage({Key key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  static Color mainBrown = Color.fromRGBO(41, 23, 34, 1.0);
+  TextStyle titleStyle =
+      TextStyle(color: mainBrown, fontSize: 20.0, fontFamily: 'CN Rocks');
   Map<String, dynamic> options = {
     "degrees": [1, 2],
     "nbStances": [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
@@ -58,6 +57,13 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void onSelectedItem(int index, List list) {
+    print('selectedIndex');
+    print(index);
+    print('list');
+    print(list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,60 +76,114 @@ class _MyHomePageState extends State<MyHomePage> {
           )),
           SafeArea(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text('Degré'),
-                _buildLists(list: options['degrees']),
-                Text('Nombre de mouvements'),
-                _buildLists(list: options['nbStances']),
-                Text('Intervals'),
-                _buildLists(list: options['nbIntervals']),
-              ],
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('$_counter'),
-                RaisedButton(
-                    child: Text('pause counter'), onPressed: _pauseCounter),
-                RaisedButton(
-                    child: Text('stop counter'), onPressed: _stopCounter)
+                SvgPicture.asset(
+                  'assets/brush_trace.svg',
+                  color: mainBrown,
+                  width: MediaQuery.of(context).size.width,
+                  height: 50.0,
+                ),
+                OptionsList(
+                  list: options['degrees'],
+                  title: 'Degré',
+                  onSelectedItem: onSelectedItem,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                OptionsList(
+                  list: options['nbStances'],
+                  title: 'Nombre de '
+                      'mouvements',
+                  onSelectedItem: onSelectedItem,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                OptionsList(
+                  list: options['nbIntervals'],
+                  title: 'Intervals',
+                  onSelectedItem: onSelectedItem,
+                ),
               ],
             ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (timer != null && timer.isActive) ? null : _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
+}
 
-  Container _buildLists({List list}) {
-    return Container(
-        height: 50.0,
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: list.length,
-            itemBuilder: (BuildContext context, int index) =>
-                Stack(children: <Widget>[
-                  Positioned.fill(
-                      child: SvgPicture.asset(
-                    'assets/splash_brush.svg',
-                    width: 150.0,
-                    height: 150.0,
-                    fit: BoxFit.cover,
-                  )),
-                  Center(
-                      child: Text('${list[index]}',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontFamily: 'CN Rocks',
-                              fontWeight: FontWeight.bold)))
-                ])));
+class OptionsList extends StatefulWidget {
+  static Color mainBrown = Color.fromRGBO(41, 23, 34, 1.0);
+  final String title;
+  final List list;
+  final Function onSelectedItem;
+  OptionsList({this.title, this.list, @required this.onSelectedItem});
+
+  @override
+  OptionsListState createState() {
+    return new OptionsListState();
+  }
+}
+
+class OptionsListState extends State<OptionsList> {
+  int _selectedItem = 0;
+
+  TextStyle titleStyle = TextStyle(
+      color: OptionsList.mainBrown, fontSize: 20.0, fontFamily: 'CN Rocks');
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 70.0),
+          child: Text(
+            widget.title,
+            style: titleStyle,
+          ),
+        ),
+        Container(
+            height: 70.0,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: widget.list.length,
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedItem = index;
+                      });
+                      widget.onSelectedItem(_selectedItem, widget.list);
+                    },
+                    child: Container(
+                        width: 130.0,
+                        height: 130.0,
+                        margin: EdgeInsets.only(left: index == 0 ? 70.0 : 0.0),
+                        child: Stack(children: <Widget>[
+                          Positioned.fill(
+                              child: SvgPicture.asset(
+                            'assets/splash_brush.svg',
+                            fit: BoxFit.cover,
+                            color: _selectedItem == index
+                                ? Colors.redAccent
+                                : OptionsList.mainBrown,
+                          )),
+                          Center(
+                              child: Text('${widget.list[index]}',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20.0,
+                                      fontFamily: 'CN Rocks',
+                                      fontWeight: FontWeight.bold)))
+                        ])),
+                  );
+                })),
+      ],
+    );
   }
 }
